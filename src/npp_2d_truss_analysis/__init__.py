5a76f41e94b016eba91ad4ea108511c0b16392d9
N. Papadakis (d-g5-5590) <npapnet@gmail.com>
2024-01-29 11:43:47 +0200

    Bumped up version number to 0.1.1 (uploaded to pypi)

--- src/npp_2d_truss_analysis/__init__.py
+++ src/npp_2d_truss_analysis/__init__.py
@@ -2,7 +2,7 @@
     @Author: N.Papadakis
     @Date: 2024/01/28
 """
-__version__ = '0.1.1a0'
+__version__ = '0.1.1'
 from npp_2d_truss_analysis.truss_input import Info, FileData,Mesh,Displacements,Forces, write_input_data
 from npp_2d_truss_analysis.truss_analysis_2d import Dofs, Analysis
 from npp_2d_truss_analysis.truss_solution import Solution, write_results
